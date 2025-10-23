20c6349dfd40e574963a858d17aa5a27363fb12d
loyde07 <loydemaylune@gmail.com>
2025-10-23 11:44:45 +0200

    refactor: update Firebase configuration to use environment variables for sensitive data #34

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -8,12 +8,12 @@ import { getStorage } from 'firebase/storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyBQmxiRC5Ms4xjrk7nqkPS0h8_E0PgZmgU',
-  authDomain: 'gentlebaby3000.firebaseapp.com',
-  projectId: 'gentlebaby3000',
-  storageBucket: 'gentlebaby3000.firebasestorage.app',
-  messagingSenderId: '407754191563',
-  appId: '1:407754191563:web:60f1b07393cd9276e8214e',
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
