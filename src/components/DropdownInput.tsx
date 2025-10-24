a8b1e36eacadaecc14b2888eab40a3879770b630
toruke <abenpro@outlook.fr>
2025-10-25 01:13:55 +0200

    US-107 dropdown fonctionnel récupère les noms des familles #51

--- src/components/DropdownInput.tsx
+++ src/components/DropdownInput.tsx
@@ -1,14 +1,7 @@
 import { Select } from 're-native-ui';
 import React from 'react';
 
-/*
-interface DropdownInputProps {
-  selectedValue : string;
-  options: { label: string; value: string }[];
-  onValueChange: (val: string) => void;
-}*/
-
-const DropdownInput = ({ selectedValue , options, onValueChange }:{selectedValue:string, options:void[],onValueChange:(val:string)=>void}) => {
+const DropdownInput = ({ selectedValue , options, onValueChange }:{selectedValue:string, options:{label: string,value:string}[],onValueChange:(val:string)=>void}) => {
 
   return(
     <Select
